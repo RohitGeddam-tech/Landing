20d831302f808ce1fa3460cc5667fb9541354a36
RohitGeddam-tech <geddamrohi13@gmail.com>
2021-06-05 19:29:24 +0530

    image compressed

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,38 @@ import React, { useRef } from "react";
 // import Button from './components/Button';
 import Header from "./components/Header";
 import Banner from "./page/Banner";
-import banner from "./images/Banner-min.jpg";
+// import banner from "./images/Banner-min.jpg";
 import Contact from "./page/Contact";
 import Lab from "./page/Lab";
 import Offer from "./page/Offer";
 import Price from "./page/Price";
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
 
 function App() {
   const myref = useRef(null);
 
   return (
     <>
-    <Helmet>
-    <link rel="preload" as="image" href={banner} />
-    </Helmet>
-    <div className="App">
-      <Header />
-      <div style={{ paddingTop: "64px" }}>
-        <Banner />
-        <Lab />
-        <Offer />
-        <Price />
-        <div id="btn">
-          <Contact />
+      <div className="App">
+        <Header />
+        <div style={{ paddingTop: "64px" }}>
+          <Banner />
+          <Lab />
+          <Offer />
+          <Price />
+          <div id="btn">
+            <Contact />
+          </div>
         </div>
-      </div>
-      <div className="footer" ref={myref} >
-        {/* <button className="blackBtn" onClick={()=>document.getElementById("btn").scrollIntoView({behavior:'smooth'})}>
+        <div className="footer" ref={myref}>
+          {/* <button className="blackBtn" onClick={()=>document.getElementById("btn").scrollIntoView({behavior:'smooth'})}>
           Get a Free Demo
         </button> */}
-        <a href="tel:18008910235" className="whiteBtn">
-          Call Us
-        </a>
+          <a href="tel:18008910235" className="whiteBtn">
+            Call Us
+          </a>
+        </div>
       </div>
-    </div>
     </>
   );
 }
