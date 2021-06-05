f8f6dacbf924c8e028997072e6e1c8c47dfb6296
RohitGeddam-tech <geddamrohi13@gmail.com>
2021-06-05 22:18:52 +0530

    image compressed

--- src/page/Banner.jsx
+++ src/page/Banner.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Banner.css";
 import banner from "../images/Banner-min.webp";
 import Button from "../components/Button";
